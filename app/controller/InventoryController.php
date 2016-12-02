d450bb7ca5c78eaf47bedcaf4509a56fa152d57a
kkzzit <kkzzit@gmail.com>
2016-12-02 21:55:10 +0100

    Update InventoryController.php

--- app/controller/InventoryController.php
+++ app/controller/InventoryController.php
@@ -4,14 +4,37 @@ class InventoryController extends Controller
     public function index()
     {
         $this->User->isNotLoggedThrowLogin();
-        
-        $this->newModel('Inventory');
-        
-        $money = Inventory::returnMoney($this->User->IDX);
-        $itemlist = Inventory::listItems($this->User->IDX);
-        
+
+        $money = Inventory::getMoney($this->User->IDX);
+
+        $equipped = [];
+        $itemlist = [];
+
+        foreach (Inventory::getInventory($this->User->IDX) as $item)
+        {
+            if ($item['itemStatus'] & 1)
+            {
+                $equipped[$item['itemSlot']] = $item;
+            }
+            else
+            {
+                array_push($itemlist, $item);
+            }
+        }
+
         require APP_VIEW . 'header.php';
         require APP_VIEW . 'inventory/inventory.php';
         require APP_VIEW . 'footer.php';
+    }
+
+    public function useitem()
+    {
+        $this->User->isNotLoggedEXIT();
+
+        $itemidx = isset($_GET['item']) && is_numeric($_GET['item']) ? $_GET['item'] : 0;
+
+        Inventory::useItem($this->User->IDX, $itemidx);
+
+        Redirect::to('inventory');
     }
 }
