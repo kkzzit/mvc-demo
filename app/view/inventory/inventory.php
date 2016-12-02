c37d4439ff34d4cc58e871286b28c51b01019f60
kkzzit <kkzzit@gmail.com>
2016-12-02 21:57:38 +0100

    Update inventory.php

--- app/view/inventory/inventory.php
+++ app/view/inventory/inventory.php
@@ -2,13 +2,41 @@
 
 <?= $this->showTitle('INVENTORY') ?>
 
-<table class="table table-condensed table-hover">
-    <?php foreach ($itemlist as $item) { ?>
-    <tr><td>
+<div class="col-xs-4 col-xs-offset-1" style="margin-bottom: 30px">
+    <table class="table table-condensed table-hover">
+        <?php foreach (Inventory::$EQUIPMENT as $equip_key => $equip) { ?>
+        <tr><td>
 
-     <strong><?= $item['itemName'] ?></strong><?= $item['itemNum'] > 1 ? ' (' . $item['itemNum'] . ')' : '' ?>
-     
-     <?= $item['itemDescr'] ? '<div class="text-medium">' . $item['itemDescr'] . '</div>' : '' ?>
+            <?php
+            print $equip['name'] . ': ';
+
+            if (array_key_exists($equip_key, $equipped) === true) {
+                $i = $equipped[$equip_key];
+
+                print '<strong>' . $this->linkCreate('inventory/useitem?item=' . $i['idx'], $i['itemName']) . '</strong>';
+            }
+            else
+            {
+                print '<i>none</i>';
+            }
+            ?>
+
+        </td></tr>
+        <?php } ?>
+    </table>
+</div>
+
+
+<table class="table table-condensed table-hover">
+
+    <?php foreach ($itemlist as $item) { ?>
+    <tr><td class="col-xs-4">
+
+        <strong><?= $item['itemType'] & 2 ? $this->linkCreate('inventory/useitem?item=' . $item['idx'], $item['itemName']) : $item['itemName'] ?></strong><?= $item['itemNum'] > 1 ? ' (' . $item['itemNum'] . ')' : '' ?>
+
+    </td><td class="col-xs-8">
+
+        <?= $item['itemDescr'] ? '<div class="text-medium">' . $item['itemDescr'] . '</div>' : '' ?>
 
     </td></tr>
     <?php } ?>
