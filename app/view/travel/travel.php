52dc51ba0680668e242a7338d55ef6c7d10475dc
kkzzit <kkzzit@gmail.com>
2017-01-11 22:58:21 +0100

    Create travel.php

---
+++ app/view/travel/travel.php
@@ -0,0 +1,28 @@
+<div class="pull-left" style="margin: 0 0 20px 0">
+    <?php if (isset($travel_info)) {?>
+    Currently traveling to: <?= $travel_info['town_to_full'] ?> (<?= $travel_info['town_to'] ?>)<br>
+    Time of arrival: <?= $travel_info['date_end'] ?><br>
+    <?php } else { ?>
+    Current location: <?= $curTownName ?>
+    <?php } ?>
+</div>
+
+
+<?= $this->showTitle('TRAVEL') ?>
+
+
+<table class="table table-hover travel">
+    <?php foreach (TOWN as $townid => $town) { ?>
+    <?php if ($this->Player->Location === $townid) continue ?>
+    <tr><td class="col-xs-1 text-center goto">
+
+      <?= $this->linkCreate('travel/travel/' . $townid, 'Travel', 'Travel', 'btn travel-btn') ?>
+
+    </td><td class="col-xs-11 town">
+
+      <strong><?= $town['name'] ?></strong><br>
+      <small>Distance: <?= GameCore::TownTravelDistance($this->Player->Location, $townid) ?>km / Time: <?= GameCore::TownTravelTimeFormatted($this->Player->Location, $townid) ?></small>
+
+    </td></tr>
+    <?php } ?>
+</table>
